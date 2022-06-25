a6ed100d2f766b5bf25ab17e9491d3b9eca1709f
kklash <k_klash@protonmail.com>
2022-06-24 19:15:14 -0700

    refactor: remove dependence on outdated constraints package

--- sort_with_values.go
+++ sort_with_values.go
@@ -1,7 +1,6 @@
 package genetic
 
 import (
-	"constraints"
 	"sort"
 )
 
@@ -12,19 +11,19 @@ const (
 	sortDescending
 )
 
-type valuedSlice[T any, V constraints.Ordered] struct {
+type valuedSlice[T any] struct {
 	Order  sortOrder
 	Items  []T
-	Values []V
+	Values []int
 }
 
 // Len implements sort.Interface. Returns the size of the valuedSlice.
-func (vs *valuedSlice[T, V]) Len() int {
+func (vs *valuedSlice[T]) Len() int {
 	return len(vs.Items)
 }
 
 // Less implements sort.Interface. Returns true if the fitness of genome i is greater than genome j.
-func (vs *valuedSlice[T, V]) Less(i, j int) bool {
+func (vs *valuedSlice[T]) Less(i, j int) bool {
 	if vs.Order == sortDescending {
 		return vs.Values[i] > vs.Values[j]
 	} else if vs.Order == sortAscending {
@@ -34,15 +33,15 @@ func (vs *valuedSlice[T, V]) Less(i, j int) bool {
 }
 
 // Swap implements sort.Interface. Swaps items of indexes i and j.
-func (vs *valuedSlice[T, V]) Swap(i, j int) {
+func (vs *valuedSlice[T]) Swap(i, j int) {
 	vs.Items[i], vs.Items[j] = vs.Items[j], vs.Items[i]
 	vs.Values[i], vs.Values[j] = vs.Values[j], vs.Values[i]
 }
 
 // sortWithValues sorts two slices. The elements of both slices are sorted in ascending or
 // descending order, as specified by order, by comparing the elements of the values slice.
-func sortWithValues[T any, V constraints.Ordered](order sortOrder, items []T, values []V) {
-	sort.Sort(&valuedSlice[T, V]{
+func sortWithValues[T any](order sortOrder, items []T, values []int) {
+	sort.Sort(&valuedSlice[T]{
 		Order:  order,
 		Items:  items,
 		Values: values,
